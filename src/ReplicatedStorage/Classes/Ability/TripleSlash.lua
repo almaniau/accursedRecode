48268974460717d2b7b3bb5debc978aa8c74432c
brianvo282 <brianvo282@gmail.com>
2026-02-01 10:50:30 -0800

    added dashing hopefully works

--- src/ReplicatedStorage/Classes/Ability/TripleSlash.lua
+++ src/ReplicatedStorage/Classes/Ability/TripleSlash.lua
@@ -1,5 +1,6 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local RunService = game:GetService("RunService")
+local ServerScriptService = game:GetService("ServerScriptService")
 local ServerStorage = game:GetService("ServerStorage")
 
 local Hitbox = require(ReplicatedStorage.Classes.Hitbox)
@@ -8,11 +9,21 @@ local Sounds = require(ReplicatedStorage.Modules.Sounds)
 local Ability = require(ReplicatedStorage.Classes.Ability)
 local Types = require(ReplicatedStorage.Classes.Types)
 local Utils = require(ReplicatedStorage.Modules.Utils)
+local CommonUtils = require(ServerScriptService.System.CommonFunctions)
 
 local TripleSlashModule = {}
 
+local function ApplyVelocity(intialVelocity : number, character : Model)
+    local velInstance = CommonUtils.ApplyVelocity(character, {InitialVelocity = intialVelocity})
+    task.delay(2, function()
+        velInstance:Destroy()
+    end)
+end
+
 local function TripleSlashBehavior(self : Types.Ability)
     if RunService:IsServer() then
+        ApplyVelocity(-20, self.OwnerProperties.Character)
+        
         task.delay(2, function() --idk change this value later
             Projectile.New({
                 SourcePlayer = self.Owner,
